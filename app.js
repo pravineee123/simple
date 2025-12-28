8eb7481ecc6439aba57cf06d124874df2451daec
pravineee123 <36792818+pravineee123@users.noreply.github.com>
2025-12-28 11:09:25 +0530

    Update app.js
    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Dockerized Node.js app!');
+  res.send('Hello from Dockerized Node.js app! Test');
 });
 
 app.listen(port, () => {
